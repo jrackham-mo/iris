b5a040c3b081c56f3582c1fc03a5dba0d3986176
stephen.worsley <stephen.worsley@metoffice.gov.uk>
2024-02-27 14:28:00 +0000

    remove benchmarks

--- benchmarks/benchmarks/regridding.py
+++ benchmarks/benchmarks/regridding.py
@@ -12,8 +12,6 @@ Regridding benchmark test
 # importing anything else
 from iris import tests  # isort:skip
 
-import tracemalloc
-
 import numpy as np
 
 import iris
@@ -60,14 +58,6 @@ class HorizontalChunkedRegridding:
         out = self.chunked_cube.regrid(self.template_cube, self.scheme_area_w)
         # Realise data
         out.data
-
-    def track_addedmem_full_regrid(self):
-        begin_snapshot = tracemalloc.take_snapshot()
-        for _ in range(8):
-            result = self.cube.regrid(self.template_cube, self.scheme_area_w)
-        end_snapshot = tracemalloc.take_snapshot()
-        diff_stats = end_snapshot.compare_to(begin_snapshot, "lineno")
-        return diff_stats[0].size_diff
 
 
 class CurvilinearRegridding:
